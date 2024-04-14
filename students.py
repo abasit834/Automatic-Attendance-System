90493af5107eee466e5feeb777666eec38289a3d
Abdul Basit <ab1438068@gmail.com>
2024-04-15 00:01:07 +0500

    modified:   students.py     resolved take picture problem

--- students.py
+++ students.py
@@ -169,7 +169,7 @@ class Students_Screen:
                 face_classifier=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
                 def face_cropped(img):
-                    gray=cv2.cvtColor(img,cv2.COLOR_BAYER_BG2BGR)
+                    gray=cv2.cvtColor(img,cv2.COLOR_BGR2BGRA)
                     faces=face_classifier.detectMultiScale(gray,1.3,5)
 
                     for(x,y,w,h) in faces:
@@ -184,16 +184,16 @@ class Students_Screen:
                     if face_cropped(my_frame) is not None:
                         img_id+=1
                         face=cv2.resize(face_cropped(my_frame),(450,450))
-                        face=cv2.cvtColor(face,cv2.COLOR_BAYER_BG2BGR)
+                        face=cv2.cvtColor(face,cv2.COLOR_BGR2BGRA)
                         file_name_path="data/user."+str(id)+"."+str(img_id)+".jpg"
                         cv2.imwrite(file_name_path,face)
                         cv2.putText(face,str(img_id),(50,50),cv2.FONT_HERSHEY_COMPLEX,2,(0,255,0),2)
                         cv2.imshow("Cropped Face",face)
                         if cv2.waitKey(1)==13 or int(img_id)==100:
                             break
-                    cap.release()    
-                    cv2.destroyAllWindows()
-                    messagebox.showinfo("Result","Generating Dataset Completed!")
+                cap.release()    
+                cv2.destroyAllWindows()
+                messagebox.showinfo("Result","Generating Dataset Completed!")
             
             except Exception as es:
                 messagebox.showerror("Error",f"Due to :{str(es)}",parent=self.root)
